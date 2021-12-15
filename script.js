c1d627682feb68109b818469a924db3f44937140
viosp <50824876+viosp@users.noreply.github.com>
2021-12-15 13:58:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function compute() {
-    var principal = +document.getElementById('principal').value || 0;
+    var principal = document.getElementById('principal').value;
     var rate = document.getElementById('rate').value;
     var years = document.getElementById('years').value;
     if (principal <= 0) {
-      alert('Please enter positve principal amount.');
-      document.getElementById('principal').focus();
-      return;
+        alert('Please enter positve principal amount');
+        document.getElementById('principal').focus();
+        return;
     }
-  
+
     var interest = (principal * years * rate) / 100;
-    var amount = principal + interest;
+    var amount = parseFloat(principal) + parseFloat(interest);
     var year = new Date().getFullYear() + parseInt(years);
     var result = document.getElementById('result');
     result.innerHTML = `If you deposit ${principal} <br>
-          at an interest rate of ${rate}% <br>
-          You will receive an amount of ${amount} <br>
-          in the year ${year}<br>`;
-  }
-  
-  function updateRate() {
+        at an interest rate of ${rate}% <br>
+        You will receive an amount of ${amount} <br>
+        in the year ${year}<br>`;
+}
+
+function updateRate() {
     var rateval = document.getElementById('rate').value;
     document.getElementById('rate_val').innerText = rateval;
-  }     
+} 
